493db483693c72cd433cac625c98c979f8d74e3b
abhg86 <76997591+abhg86@users.noreply.github.com>
2022-07-15 22:38:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import chess
 import chess.syzygy
-import chess.gaviota
-import sklearn 
 import random as rd
 import math
-import time 
+import numpy as np 
+import csv
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
-from itertools import permutations
+import sklearn.metrics as metrics
+# from itertools import permutations
 
 def create_board(pieces,places, color):
     board = chess.Board()
@@ -30,26 +30,6 @@ def random_board(pieces):
         return random_board(pieces)
     else:
         return board
-
-def generate_boards(pieces, n):
-    for i in range(n):
-        yield random_board(pieces)
-
-def generate_all(pieces):
-    for positions in permutations(chess.SQUARES, len(pieces)):
-        board1 = create_board(pieces, positions, 'w')
-        board2 = create_board(pieces, positions, 'w')
-        if board1.is_valid():
-            yield board1
-        if board2.is_valid():
-            yield board2 
-
-def generate_all_solutions(pieces):
-    with chess.syzygy.open_tablebase("data/syzygy/wdl") as tablebase :
-        for board in generate_all(pieces):
-            yield tablebase.probe_wdl(board)
-
-
 
 def pieces_score(board):
     description = board.fen()
@@ -132,11 +112,15 @@ def features(board):
     pieces = pieces_score(board)
     return [board.turn, pieces[0], pieces[1], pieces[2], pieces[3], pieces[4], pieces[5], pieces[6], pieces[7], pieces[8], pieces[9], int(board.is_check()), nb_wmoves(board), nb_bmoves(board), nb_wattack(board), nb_battack(board), nb_wpin(board), nb_bpin(board)]
 
-def train_set(nb, pieces):
+def train_set(nb, nb_pieces, pieces=None):
     Xtr, Ytr = [], []
     boards = []
     with chess.syzygy.open_tablebase("data/syzygy/wdl") as tablebase :
         for i in range(nb):
+            if not pieces :
+                pieces = np.random.choice(['Q','q','R','R','r','r','B','B','b','b','N','N','n','n','P','p','P','p','P','p','P','p','P','p','P','p','P','p','P','p'], nb_pieces -2, replace=False)
+                pieces = list(pieces)
+                pieces += ['k','K']
             board = random_board(pieces)
             boards += [board]
 
@@ -146,95 +130,59 @@ def train_set(nb, pieces):
             Ytr += [result]
     return Xtr, Ytr, boards
 
-def test_set(nb, pieces, trainset):
-    Xte, Yte = [], []
-    with chess.syzygy.open_tablebase("data/syzygy/wdl") as tablebase :
-        i = 0
-        while i < nb:
-            board = random_board(pieces)
-            if not board in trainset:
-                i += 1
-                feature = features(board)
-                result = tablebase.probe_wdl(board)
-                Xte += [feature]
-                Yte += [result]
-        tablebase.close()
-    return Xte, Yte
-
-# X, Y, boards = train_set(1000, ['K', 'k', 'Q', 'r'])
-# Xt, Yt = test_set(200, ['K', 'k', 'Q', 'r'], boards)
-
-# clf = LogisticRegression(random_state=0).fit(X, Y)
-# board, result = Xt[0], Yt[0]
-# print(clf.score(Xt,Yt))
-# print(result)
-# print(clf.predict_proba([board]))
-
-
-def bounds(clf, pieces):
-    nb_right_ans = 0
-    n = 0
-
-    somme = 0
-    with chess.syzygy.open_tablebase("data/syzygy/wdl") as tablebase :
-        for board in generate_all(pieces):
-            n+=1
-            pos = tablebase.probe_wdl(board)
-            if pos == clf.predict([features(board)])[0]:
-                nb_right_ans += 1
-
-            if pos == 0:                            #because probe_wdl return -2, 0 or 2
-                pos = 1
-            elif pos == -2:
-                pos = 0
-            somme -= clf.predict_log_proba([features(board)])[0][pos]
-
-    acc = nb_right_ans / n
-    return ((1-acc)* n * (math.log(n)+2), somme) 
-
-def n_naif(k):
-    n=64
-    for i in range(64-k+1, 64):
-        n *= i
-    return n
-
-
 def partial_bounds(clf, Xt, Yt, N):
     nb_right_ans = 0
-    somme = 0
-    for x,y in zip(Xt,Yt):
-        if y == clf.predict([x])[0]:
-            nb_right_ans += 1
-         
+    log_proba = []
+    for x,y in zip(clf.predict_proba(Xt),Yt):
         if y == 0:                            #because probe_wdl return -2, 0 or 2
             pos = 1
         elif y == -2:
             pos = 0
         else :
             pos = 2
-        somme -= clf.predict_log_proba([x])[0][pos]
 
+        if pos == np.argmax(x):
+            nb_right_ans += 1
+         
+        log_proba += [-math.log2(x[pos])]
+
+    log_proba = np.array(log_proba)
     n = len(Xt)
     acc = nb_right_ans / n
-    return ((1-acc)* N * (math.log(N)+2), N * 1/n * somme)
+    moyenne = log_proba.mean()
+    return ((1-acc)* N * (math.log(N)+2), N * moyenne, moyenne, log_proba.std())
 
-N = n_naif(4)
-board_tot = []
-clf = SGDClassifier(loss = 'log')
-for i in range(100):                                    #arbitrary, can be changed 
-    X, Y, _ = train_set(1000, ['K', 'k', 'Q', 'r'])
-    clf.partial_fit(X, Y, classes = [-2,0,2])
-    if i%10 == 0 and i>20 :                                        #i > 20 in order to avoid the log error    
-        Xt, Yt, _ = train_set(1000, ['K', 'k', 'Q', 'r'])          #actually a test set but no verification is needed so train_set is more appropriate
-        borne1, borne2 = partial_bounds(clf, Xt, Yt, N)
-        print(i, " :")
-        print("borne 1 : ", borne1)
-        print("borne 2 : ", borne2)
+def main():
+    N = 1677216                   # number of boards possible with 4 pieces
+    clf = SGDClassifier(loss = 'log')
+    with open('data3.csv', 'a', newline='') as fichiercsv:
+        w = csv.writer(fichiercsv)
+        w.writerow(['New data'])
+        w.writerow(['Step', 'metric', 'value'])
+        for i in range(10000):                                    #arbitrary, can be changed 
+            X, Y, _ = train_set(1000, 4)
+            clf.partial_fit(X, Y, classes = [-2,0,2])
+            if i%10 == 0 and i>20 :                                        #i > 20 in order to avoid the log error    
+                Xt, Yt, _ = train_set(1000, 4)          #actually a test set but no verification is needed so train_set is more appropriate
+                borne1, borne2, moy, ec_type = partial_bounds(clf, Xt, Yt, N)
 
-Xt, Yt, _ = train_set(10000, ['K', 'k', 'Q', 'r'])         #actually a test set but no verification is needed so train_set is more appropriate
+                Ypred = clf.predict(Xt)                                     # faster than doing list(map(np.argmax, y)) with y being clf.predict_proba(Xt) precalculted for partial bounds
+                conf_mat = metrics.confusion_matrix(Yt, Ypred)
 
-borne1, borne2 = partial_bounds(clf, Xt, Yt, N)
-print()
-print("final")
-print("borne 1 : ", borne1)
-print("borne 2 : ", borne2)
+                accuracy = clf.score(Xt,Yt)
+
+                w.writerows([[i, 'bound1', borne1], [i, 'bound2', borne2], [i, 'mean', moy], [i, 'standard deviation', ec_type], [i, 'accuracy', accuracy], [i, 'confusion matrix', conf_mat],])
+
+        Xt, Yt, _ = train_set(10000, 4)         #actually a test set but no verification is needed so train_set is more appropriate
+
+        borne1, borne2, moy, ec_type = partial_bounds(clf, Xt, Yt, N)
+
+        Ypred = clf.predict(Xt)                                     # faster than doing list(map(np.argmax, y)) with y being clf.predict_proba(Xt) precalculted for partial bounds
+        conf_mat = metrics.confusion_matrix(Yt, Ypred)
+
+        accuracy = clf.score(Xt,Yt)
+    
+        w.writerows([['final', 'bound1', borne1], ['final', 'bound2', borne2], ['final', 'mean', moy], ['final', 'standard deviation', ec_type], ['final', 'accuracy', accuracy], ['final', 'confusion matrix', conf_mat],])
+
+
+main()
